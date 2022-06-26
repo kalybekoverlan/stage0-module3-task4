b7403fb83f08dea8df8aa9cbac22157afc508d7b
erlan <kalybekov.erlan@gmail.com>
2022-06-26 21:03:44 +0600

    module 3 task 4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int letterC = 99, letterH = 104, letterA = 97, letterR = 114;
+        System.out.println( (char) letterC);
+        System.out.println( (char) letterH);
+        System.out.println( (char) letterA);
+        System.out.println( (char) letterR);
     }
 }
